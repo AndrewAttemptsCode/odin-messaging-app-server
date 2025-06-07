c139080d2d64287f83d007bb7328d46d5fadc31d
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-06-07 18:13:50 +0100

    feat: add create new user logic

---
+++ src/controllers/user.js
@@ -0,0 +1,27 @@
+const bcrypt = require("bcryptjs");
+const prisma = require("../../configs/prismaClient");
+const asyncHandler = require("express-async-handler");
+const { validationResult } = require("express-validator");
+
+const createUser = asyncHandler(async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { username, password } = req.body;
+
+  const hashedPassword = bcrypt.hash(password, 10);
+
+  await prisma.user.create({
+    data: {
+      username,
+      password: hashedPassword,
+    },
+  });
+});
+
+module.exports = {
+  createUser,
+};
