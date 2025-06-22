fb7a9cfbd93eac8cb79d3cd59f3ac4d8b51e6499
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2025-06-22 15:36:03 +0100

    feat: add route to update user model with user settings

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const controllers = require("../controllers/index");
 const registerValidation = require("../middlewares/registerValidation");
+const authenticateUser = require("../middlewares/authenticateUser");
 
 const router = Router();
 
 router.post("/", registerValidation, controllers.user.createUser);
 router.get("/", controllers.user.getAllUsers);
+router.put("/:userId", authenticateUser, controllers.user.updateUserSettings);
 
 module.exports = router;
